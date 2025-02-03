5bf88f4651e39c045fbd6b35f5ae824d62995b20
jacegonzales25 <jacegonzales25@gmail.com>
2025-02-04 02:30:25 +0800

    fix: separate concerns of db outside nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'assets.example.com',
+          port: '',
+          pathname: '/account123/**',
+          search: '',
+        },
+      ],
+    },
   output: 'standalone'
 };
 
